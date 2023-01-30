014af6ef7de469cc238077a9df50e34acad6a9d9
bunny204 <shavondra.lynch@gmail.com>
2023-01-29 19:32:48 -0600

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import { React } from "react";
-import { ReactDOM } from "react-dom/client";
-import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import Weather from "./Weather";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <Weather city="Dallas" />
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
